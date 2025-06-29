b5325381547f79c70728da7b8bfba62ffeaacca1
Arthur Gatignol <arthur.gatignol@efrei.net>
2025-06-29 17:58:43 +0200

    corrected the api

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ import pickle
 app = FastAPI()
 model = pickle.load(open("models/random_forest_model/random_forest_model_2/model.pkl", "rb"))
 
+from fastapi import Query
+
 @app.get("/predict")
-def predict(x: float):
+def predict(x: float = Query(..., title="Input value")):
     y = model.predict([[x]])
     return {"input": x, "prediction": float(y[0])}
